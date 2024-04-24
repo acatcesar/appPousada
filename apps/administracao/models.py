2d0d7a82fcf0b8336a88d58e690f882b7c379352
acatcesar <augusto.cesar.tavares.a@gmail.com>
2024-04-23 21:45:19 -0300

    noot

---
+++ apps/administracao/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class Apartamento(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField
+    numero = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+class Reserva(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, null=True, blank=True)
+    valor = models.DecimalField
+    dataEntrada= models.DateTimeField
+    dataSaida = models.DateTimeField
+    qntDias = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.PROTECT, null=True, blank=True)
+    apartamento = models.ForeignKey(Apartamento, on_delete=models.PROTECT, null=True, blank=True, db_constraint=False)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+
+class Cupons(models.Model):
+    codigo = models.CharField(max_length=50)
+    desconto = models.IntegerField(default=0)
+    utilizado = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.codigo
+
+
+# class Usuario(AbstractUser):
+#     cupons_utilizados = models.ManyToManyField("Cupons")
+
+
+

3f5afe8a737eed9a42d89888763596de2959107c
Augusto Cesar Albuquerque Tavares <88669775+acatcesar@users.noreply.github.com>
2024-07-10 00:01:35 -0300

    ajustes apresentação

--- appPousada/settings.py
+++ appPousada/settings.py
@@ -70,22 +70,22 @@ WSGI_APPLICATION = 'appPousada.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'appPousada',
-#         'USER': 'postgres',
-#         'PASSWORD': 'auau102030',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appsPousada',
+        'USER': 'postgres',
+        'PASSWORD': 'auau102030',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
